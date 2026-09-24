022e0aa6907f4d29be11fc6c55c964b3191a38d2
Olt <80402091+oltinios@users.noreply.github.com>
2026-09-25 00:02:58 +0100

    Testing detection of Resolve.exe

---
+++ program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+while (true)
+{
+    bool resolveRunning = Process.GetProcessesByName("Resolve").Length > 0;
+
+    if (resolveRunning)
+    {
+        Console.WriteLine("DaVinci Resolve is running");
+    }
+    else
+    {
+        Console.WriteLine("DaVinci Resolve is not running");
+    }
+
+    Thread.Sleep(10000);
+}
